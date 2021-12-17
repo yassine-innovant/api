eb5a52a6a61b2f0cc0e46d1181db595301b5f692
GoldenPaladin <yacine.ezdini@gmail.com>
2021-12-17 10:34:10 +0100

    User Model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+//user schema
+const userSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    trim: true,
+    required: true,
+    max: 32,
+  },
+  last_name: {
+    type: String,
+    trim: true,
+    required: true,
+    max: 32,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+    lowercase: true,
+    index: true,
+  },
+  crypted_password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
